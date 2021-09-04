88c0be998328e53c8b93917652aacb4eedf9f0c7
atif shafi <atifshafi63@gmail.com>
2021-09-04 14:25:28 +0530

    updated settings

--- food_deliveryapp/settings.py
+++ food_deliveryapp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import environ
-from .secret import key
+# from .secret import key
 
 env = environ.Env()
 environ.Env.read_env()
