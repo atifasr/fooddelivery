4a416b2030005bc1b6619c96bbbd1987546d471d
atif shafi <atifshafi63@gmail.com>
2021-09-05 12:49:19 +0530

    added hroku data base config

--- food_deliveryapp/settings.py
+++ food_deliveryapp/settings.py
@@ -201,16 +201,9 @@ RAZOR_SEC_KEY = 'p79UdplbOgurzcMJDRqMHxjW'
 # DATABASES['default'] = dj_database_url.config()
 
 
-# in_heroku = False
-# if 'DATABASE_URL' in os.environ:
-#     in_heroku = True
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
 
-# if in_heroku:
-#     DATABASES = {'default': dj_database_url.config()}
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
+if in_heroku:
+   DATABASES['default'] = dj_database_url.config()
